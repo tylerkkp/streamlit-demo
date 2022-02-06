9ea2b97871102d08141f9f44b67c6650705e7fb8
Tyler K Kuromiya Parker <tylerkkp@gmail.com>
2022-02-05 16:57:53 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,26 @@
 import streamlit as st
 
-count = 0
-counter = st.write(count)
+# Python program to generate WordCloud
 
-def hello():
-  count += 1
-  
-st.button('Click Me!', on_click=hello())
+# importing all necessary modules
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def word_cloud(text):
+  # Create and generate a word cloud image:
+  wordcloud = WordCloud(width = 800, height = 800,
+          background_color ='white',
+          stopwords = stopwords,
+          min_font_size = 10).generate(text)
+
+  # Display the generated image:
+  plt.figure() 
+  plt.imshow(wordcloud, interpolation='bilinear')
+  plt.axis("off")
+  plt.show()
+
+txt = st.text_area('Text:')
+
+if st.button('Create Word Cloud'):
+  st.write(word_cloud(txt))
