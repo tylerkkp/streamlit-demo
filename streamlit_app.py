b35decb60fe8000d013ac0287bbdb2c763d4c9cb
Tyler K Kuromiya Parker <tylerkkp@gmail.com>
2022-02-05 22:28:34 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,26 @@ if clear:
     st.session_state.count = 0
 
 st.write('Count = ', st.session_state.count)
+
+import pandas as pd
+import numpy as np
+import altair as alt
+
+st.title('Graph Normal Distribution')
+if 'points' not in st.session_state:
+    st.session_state.points = 0
+    
+points = st.slider('Samples', 1, 500)
+
+draw = st.button('Sample and Plot')
+if draw:
+    xdata = pd.DataFrame(np.random.normal(1, 100, st.session.state.points))
+    ydata = pd.DataFrame(np.random.normal(1, 100, st.session.state.points))
+    df = xdata.join(ydata)
+    df.columns = ['x', 'y']
+
+    c = alt.Chart(df).mark_circle().encode(
+         x='x', y='y', tooltip=['a', 'b'])
+
+st.altair_chart(c, use_container_width=True)
+                 
