d3972fd92db017fa5db50531ddf3a7417ebc5116
Tyler K Kuromiya Parker <tylerkkp@gmail.com>
2022-02-05 17:30:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,14 @@ st.title('Counter Example')
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
-amount = st.radio('Amount', [1, 5, 10, 20, 50, 100])
+amount = st.slider('Amount', 1, 50)
     
 increment = st.button('Increment')
 if increment:
     st.session_state.count += amount
+
+clear = st.button('Clear')
+if clear:
+    st.session_state.count = 0
 
 st.write('Count = ', st.session_state.count)
